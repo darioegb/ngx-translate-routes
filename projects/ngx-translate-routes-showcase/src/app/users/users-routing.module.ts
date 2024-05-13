f7d3d6417499d8458d17dd54a0bc5d7744fc647c
darioegb <darioegb@gmail.com>
2024-05-13 09:19:36 -0300

    fix: example breaking change routing

--- projects/ngx-translate-routes-showcase/src/app/users/users-routing.module.ts
+++ projects/ngx-translate-routes-showcase/src/app/users/users-routing.module.ts
@@ -4,7 +4,7 @@ import { ProfileComponent } from '../profile/profile.component';
 import { ListusersComponent } from '../listusers/listusers.component';
 
 const routes: Routes = [
-  { path: '', data: {title: 'titles.users.root'} },
+  { path: '', children: [], data: {title: 'titles.users.root'} },
   { path: 'profile/:id', component: ProfileComponent, data: {title: 'titles.users.profile'} },
   { path: 'myaccount', component: ListusersComponent, data: {title: 'titles.users.myaccount'} }
 ];
