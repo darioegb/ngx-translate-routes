6032990e95623b6a4e5bea6acaa7fb01916343c3
darioegb <darioegb@gmail.com>
2024-07-17 19:04:27 -0300

    fix: solve problem lost query params when refresh browser

--- projects/ngx-translate-routes-showcase/src/app/about/about.component.ts
+++ projects/ngx-translate-routes-showcase/src/app/about/about.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom'
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  #activatedRoute = inject(ActivatedRoute)
+
+  async ngOnInit(): Promise<void> {
+    const params = await firstValueFrom(this.#activatedRoute.queryParams)
+    const name = params['name']
+    console.log(name)
+  }
+}
