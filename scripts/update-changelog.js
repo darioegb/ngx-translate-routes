5f02720083237b3999f9c2ac47eb798354f6af39
Dario Gonzalez <darioegb@gmail.com>
2025-03-03 15:53:15 -0300

    fix: changelog

--- scripts/update-changelog.js
+++ scripts/update-changelog.js
@@ -2,15 +2,7 @@ const fs = require('fs');
 const execSync = require('child_process').execSync;
 
 const changelogPath = 'CHANGELOG.md';
-let changelogContent = '';
 
-if (fs.existsSync(changelogPath)) {
-  changelogContent = fs.readFileSync(changelogPath, 'utf8');
-}
+const newChangelog = execSync("npx auto-changelog --stdout").toString();
 
-const { version: latestVersion } = JSON.parse(fs.readFileSync('projects/ngx-translate-routes/package.json', 'utf8'));
-
-const newChangelog = execSync(`npx auto-changelog --stdout --latest-version "${latestVersion}"`).toString();
-
-const updatedChangelogContent = `${newChangelog}\n${changelogContent}`;
-fs.writeFileSync(changelogPath, updatedChangelogContent);
+fs.writeFileSync(changelogPath, newChangelog);
